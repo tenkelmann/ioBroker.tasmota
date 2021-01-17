5802e07a1ed5b955bb23bc0f15ddae6b1eb8c6ac
tenkelmann <t.enkelmann@web.de>
2021-01-17 13:48:57 +0100

    Erste Aufräumarbeiten

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 "use strict";
 
-/*
- * Created with @iobroker/create-adapter v1.31.0
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
