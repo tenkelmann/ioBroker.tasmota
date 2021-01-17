3103d53f597120ee2715481fadc23aae5f4d1b52
tenkelmann <t.enkelmann@web.de>
2021-01-17 12:45:00 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"tasmota adapter settings": {
+		"en": "Adapter settings for tasmota",
+		"de": "Adaptereinstellungen für tasmota",
+		"ru": "Настройки адаптера для tasmota",
+		"pt": "Configurações do adaptador para tasmota",
+		"nl": "Adapterinstellingen voor tasmota",
+		"fr": "Paramètres d'adaptateur pour tasmota",
+		"it": "Impostazioni dell'adattatore per tasmota",
+		"es": "Ajustes del adaptador para tasmota",
+		"pl": "Ustawienia adaptera dla tasmota",
+		"zh-cn": "tasmota的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
